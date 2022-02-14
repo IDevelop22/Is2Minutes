494c7b8ece6245c3db3ac2090c834c6d77c7c3a7
I Develop <katlegobenlephadi@gmail.com>
2022-02-14 19:24:09 +0200

    initial commit

---
+++ Extensions/StartupExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Is2MinutesBackend.PostgreSQL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Is2MinutesBackend.Extensions
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHost MigrateDatabase(this IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetService<BloggingContext>();
+                    context.Database.Migrate(); ;
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred migrating the database.");
+                }
+            }
+
+            return host;    
+
+        }
+    }
+}
